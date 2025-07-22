eec9a5dff42fe386a9922a4e438578b409043c0f
Areesha Kaleem <145961765+Areesha-Kaleem@users.noreply.github.com>
2025-07-22 15:50:49 +0500

    Update main_load.py

--- loader/main_load.py
+++ loader/main_load.py
@@ -5,14 +5,22 @@ def load():
     final_json = "D:\\LegalMorph\\final_json"
     custom_json = "D:\\LegalMorph\\custom_json"
     base_json = "D:\\LegalMorph\\base_json"
+    raw_dir = "D:\\LegalMorph\\data"
+    summarized_dir = "D:\\LegalMorph\\summarized_text"
     f_name = "final"
     f_collection = "LegalCases"
     c_name = "Custom"
     c_collection = "CustomLegalCases"
     b_name = "Base"
     b_collection = "BaseLegalCases"
+    s_name = "Summarized"
+    s_collection = "SumLegalCases"
+    r_name = "Raw"
+    r_collection = "Legal_raw_cases"
     print("Loading Raw json in DB...")
-    txt_json_db()
+    txt_json_db(raw_dir, r_name, r_collection)
+    print("Loading Summarized json for long cases in DB...")
+    txt_json_db(summarized_dir, s_name, s_collection)
     print("loading custom json...")
     load_json(custom_json, c_name, c_collection)
     print("loading base json...")
